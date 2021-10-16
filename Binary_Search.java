c45ec3108aaa248b2359dbab3ff62221aaac6017
Aliza Dixit <alizadixit6@gmail.com>
2021-10-16 21:49:15 +0530

    update binary search java

--- Binary_Search.java
+++ Binary_Search.java
@@ -1,46 +1,25 @@
 //BINARY SEARCH PROGRAM IN JAVA
 
 import java.util.Scanner;
-class BinarySearchExample
-{
-   public static void main(String args[])
-   {
-      int counter, num, item, array[], first, last, middle;
-      //To capture user input
-      Scanner input = new Scanner(System.in);
-      System.out.println("Enter number of elements:");
-      num = input.nextInt(); 
-
-      //Creating array to store the all the numbers
-      array = new int[num];
-
-      System.out.println("Enter " + num + " integers");
-      //Loop to store each numbers in array
-      for (counter = 0; counter < num; counter++)
-          array[counter] = input.nextInt();
-
-      System.out.println("Enter the search value:");
-      item = input.nextInt();
-      first = 0;
-      last = num - 1;
-      middle = (first + last)/2;
-
-      while( first <= last )
-      {
-         if ( array[middle] < item )
-           first = middle + 1;
-         else if ( array[middle] == item )
-         {
-           System.out.println(item + " found at location " + (middle + 1) + ".");
-           break;
-         }
-         else
-         {
-             last = middle - 1;
-         }
-         middle = (first + last)/2;
-      }
-      if ( first > last )
-          System.out.println(item + " is not found.\n");
-   }
-}
+import java.util.*;
+public class binary{    
+public static void toBinary(int decimal){    
+     int binary[] = new int[40];    
+     int index = 0;    
+     while(decimal > 0){    
+       binary[index++] = decimal%2;    
+       decimal = decimal/2;    
+     }    
+     for(int i = index-1;i >= 0;i--){    
+       System.out.print(binary[i]);    
+     }    
+System.out.println();//new line  
+}    
+public static void main(String args[]){      
+System.out.println("Decimal of 3 is: ");  
+toBinary(3);    
+System.out.println("Decimal of 21 is: ");  
+toBinary(21);    
+System.out.println("Decimal of 31 is: ");    
+toBinary(31);  
+}}      
