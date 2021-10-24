9ceb27304d9174733cf71bc1d0e6df14e296a005
Chetan Chavan <65353606+chetan355@users.noreply.github.com>
2021-10-24 10:35:12 +0530

    swapping without using extra variable

--- java_codes/Bubble_Sort.java
+++ java_codes/Bubble_Sort.java
@@ -10,9 +10,9 @@ class BubbleSort
 				if (arr[j] > arr[j+1])
 				{
 					// swap arr[j+1] and arr[j]
-					int temp = arr[j];
-					arr[j] = arr[j+1];
-					arr[j+1] = temp;
+					arr[j] = arr[j] + arr[j+1];
+					arr[j+1] = arr[j] - arr[j+1];
+					arr[j] = arr[j] - arr[j+1];
 				}
 	}
 
